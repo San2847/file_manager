429a2d616c613e54f4634cd2c7917ebe0a5fe05e
Praveer <praveer.csgo2016@gmail.com>
2023-02-23 10:44:46 +0530

    new build

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/file-beta/">
           <App />
         </BrowserRouter>
       </PersistGate>
