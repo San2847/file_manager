2afff09e250a6623f9e791f7a14ae92816cb33b6
shivamvermaquestglt <shivamverma@questglt.org>
2023-05-11 15:58:10 +0530

    update in file manager landing page '

--- src/Pages/Homepage/HomepageWeb/FileLanding/FileLanding.jsx
+++ src/Pages/Homepage/HomepageWeb/FileLanding/FileLanding.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Breadcrumb from "../../../../Components/Common/Breadcrumb/Breadcrumb";
 import styles from "./fileLanding.module.css";
+import task from "../../../../Assets/task.svg";
+import rate_review from "../../../../Assets/rate_review.svg";
 import { BsSearch } from "react-icons/bs";
 import { Table } from "react-bootstrap";
 import { getReq } from "../../../../Services/api";
@@ -16,7 +18,9 @@ const FileLanding = () => {
   const [projectFileData, setProjectFileData] = useState([]);
   const [filteredProjectData, setFilteredProjectData] = useState([]);
   const getProjectFileData = async () => {
-    const res = await getReq(`${apiLinks.pmt}/api/file-manager/details-with-project`);
+    const res = await getReq(
+      `${apiLinks.pmt}/api/file-manager/details-with-project`
+    );
     if (res && !res.error) {
       setProjectFileData([...res.data]);
     } else {
@@ -50,7 +54,7 @@ const FileLanding = () => {
           <Breadcrumb
             pathArr={JSON.stringify([
               { label: "Home", link: "" },
-              { label: "Files", navTo: "" },
+              { label: "Files", navTo: "" }
             ])}
           />
         </div>
@@ -62,10 +66,19 @@ const FileLanding = () => {
           <div className="d-flex align-items-center">
             {showInput && (
               <div className={styles.searchInput}>
-                <input type="text" value={searchText} onChange={(event) => setSearchText(event.target.value)} />
+                <input
+                  type="text"
+                  value={searchText}
+                  onChange={(event) => setSearchText(event.target.value)}
+                />
               </div>
             )}
-            <div onClick={() => setShowInput(!showInput)} className={showInput ? styles.searchActive : styles.searchInactive}>
+            <div
+              onClick={() => setShowInput(!showInput)}
+              className={
+                showInput ? styles.searchActive : styles.searchInactive
+              }
+            >
               {showInput ? <AiOutlineClose /> : <BsSearch />}
             </div>
           </div>
@@ -74,16 +87,24 @@ const FileLanding = () => {
           <Table bordered hover>
             <thead>
               <tr>
-                <th style={{ fontSize: "14px" }}>PROJECT NAME</th>
+                <th style={{ fontSize: "14px", width: '25%' }}>PROJECT NAME</th>
                 <th style={{ fontSize: "14px" }}>TOTAL FILES</th>
                 <th style={{ fontSize: "14px" }}>APPROVED</th>
                 <th style={{ fontSize: "14px" }}>IN-DISCUSSION</th>
                 <th style={{ fontSize: "14px" }}>IN-EXECUTION</th>
+                <th
+                  style={{ fontSize: "14px" }}
+                  className="d-flex justify-content-center"
+                >
+                  Recent Activity
+                </th>
+                <th style={{ fontSize: "14px" }}>Last Update</th>
               </tr>
             </thead>
             <tbody>
               {filteredProjectData &&
                 filteredProjectData.map((curElem) => {
+                  console.log(curElem);
                   return (
                     <tr
                       style={{ cursor: "pointer" }}
@@ -96,6 +117,23 @@ const FileLanding = () => {
                       <td>{curElem.allFiles}</td>
                       <td>{curElem.approvedFiles}</td>
                       <td>{curElem.inDiscussion}</td>
+                      <td>{curElem.inExecution}</td>
+                      <td className="d-flex justify-content-between">
+                        <div>
+                          <img src={rate_review} alt="" style={{ marginRight: "0.5rem" }} />
+                          <span style={{ fontWeight: 500, fontSize: "12px" }}>
+                            {curElem.inExecution}
+                            {"Feedback"}
+                          </span>
+                        </div>
+                        <div>
+                          <img src={task} alt="" style={{ marginRight: "0.5rem" }} />
+                          <span style={{ fontWeight: 500, fontSize: "12px" }}>
+                            {curElem.inExecution}
+                            {"Approval"}
+                          </span>
+                        </div>
+                      </td>
                       <td>{curElem.inExecution}</td>
                     </tr>
                   );
